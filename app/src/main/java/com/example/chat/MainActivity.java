632576fbc15ef213ea727bf0d7e81b59f75b6846
DnkDm <97932352+DnkDm@users.noreply.github.com>
2022-12-10 03:04:03 +0500

    /

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.utils.widget.ImageFilterButton;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView messageListView;
+    private AwesomeMessageAdapter adapter;
+    private ProgressBar progressBar;
+    private ImageButton sendImageButton;
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
