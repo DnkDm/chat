60d46b8ceb85788b534ea4e168de36dce4893ce1
DnkDm <97932352+DnkDm@users.noreply.github.com>
2022-12-10 20:05:32 +0500

    /

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.chat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.utils.widget.ImageFilterButton;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +34,20 @@ public class MainActivity extends AppCompatActivity {
     private Button sendMessageButton;
     private EditText messageEditText;
 
+    FirebaseDatabase database;
+    DatabaseReference messageDatabaseReference;
+    ChildEventListener messagesChildEventListener;
+
     private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        messageDatabaseReference = database.getReference().child("messages");
+
 
         progressBar = findViewById(R.id.progressBar);
         sendImageButton = findViewById(R.id.sendPhotoButton);
@@ -45,11 +64,38 @@ public class MainActivity extends AppCompatActivity {
 
         progressBar.setVisibility(ProgressBar.INVISIBLE);
 
-        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+        messageEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0){
+                    sendMessageButton.setEnabled(true);
+                } else {
+                    sendMessageButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        messageEditText.setFilters(new InputFilter[] {new InputFilter.LengthFilter(500)});
+
+        sendMessageButton.setOnClickListener(view -> {
+
+            AwesomeMessage message = new AwesomeMessage();
+            message.setText(messageEditText.getText().toString());
+            message.setName(userName);
+            message.setInageUrl(null);
+
+            messageDatabaseReference.push().setValue(message);
+
+            messageEditText.setText("");
         });
 
         sendImageButton.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +104,36 @@ public class MainActivity extends AppCompatActivity {
                 
             }
         });
+
+        messagesChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                AwesomeMessage message = snapshot.getValue(AwesomeMessage.class);
+                adapter.add(message);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+
+        messageDatabaseReference.addChildEventListener(messagesChildEventListener);
 
     }
 }
