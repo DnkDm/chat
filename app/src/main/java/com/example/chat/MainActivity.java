4d4504b356d370c220d2d08c2c961106af308c76
DnkDm <bolgam148@gmail.com>
2022-12-10 17:43:31 +0500

    /

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.utils.widget.ImageFilterButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +20,44 @@ public class MainActivity extends AppCompatActivity {
     private AwesomeMessageAdapter adapter;
     private ProgressBar progressBar;
     private ImageButton sendImageButton;
-    
+    private Button sendMessageButton;
+    private EditText messageEditText;
 
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        sendImageButton = findViewById(R.id.sendPhotoButton);
+        sendMessageButton = findViewById(R.id.sendMassegeButton);
+        messageEditText = findViewById(R.id.messageEditText);
+
+
+        userName = "Default User";
+
+        messageListView = findViewById(R.id.messageListView);
+        List<AwesomeMessage> awesomeMessages = new ArrayList<>();
+        adapter = new AwesomeMessageAdapter(this, R.layout.message_item, awesomeMessages);
+        messageListView.setAdapter((adapter));
+
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sendImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
